673e02cda7e902acaa91581e618cb116cae722fc
Ami13F <ameliaforgacs@gmail.com>
2020-05-08 19:07:31 +0300

    [feat] Change yolo to yolo-tiny

---
+++ ModelYoloImpl/yolov3_tf2/main.py
@@ -0,0 +1,125 @@
+import tensorflow as tf
+from utils import load_class_names, output_boxes, draw_output, resize_image, transform_images
+import cv2
+import numpy as np
+from yolov3 import YoloV3
+import os.path
+
+# physical_devices = tf.config.experimental.list_physical_devices('GPU')
+# assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+# tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+
+def detect_image(detect, class_names_file, model_size, model):
+    print("Detecting objects loading....")
+    if detect == False:
+        return
+
+    image_file = "bottle2.jpeg"
+    img_path = os.path.abspath("yolov3_tf2\\" + image_file)
+    class_names = load_class_names(class_names_file)
+
+    img_raw = tf.image.decode_image(
+        open(img_path, 'rb').read(), channels=3)
+
+    img = tf.expand_dims(img_raw, 0)  # create one more dimension for batch (1, 416, 416, 3)
+    print(img.shape)
+    img = transform_images(img, model_size[0])  # 416 size
+    # boxes, classes, scores, nums = model.predict(resized_frame)
+    boxes, classes, scores, nums = model(img)
+    for i in range(nums[0]):
+        print('\t{}, {}, {}'.format(class_names[int(classes[0][i])],
+                                    np.array(scores[0][i]),
+                                    np.array(boxes[0][i])))
+
+    # image = np.squeeze(image)
+    img = cv2.cvtColor(img_raw.numpy(), cv2.COLOR_RGB2BGR)
+    img = draw_output(img, boxes, scores,
+                      classes, nums, class_names)
+    win_name = 'Detected objects'
+    cv2.imshow(win_name, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    # For saving the result
+    cv2.imwrite(os.path.abspath('yolov3_tf2\\test\\test0.jpg'), img)
+    print("Image saved....")
+
+
+def convert(model, model_name):
+    print("Converting model to tflite.....")
+
+    # image_file = "people.jpg"
+    # img_path = os.path.abspath("yolov3_tf2\\" + image_file)
+    # class_names = load_class_names(class_names_file)
+    # image = cv2.imread(img_path)
+    # image = np.array(image)
+    # image = tf.expand_dims(image, 0)
+    # resized_frame = resize_image(image, (model_size[0], model_size[1]))
+
+    # model.predict(resized_frame)
+
+    converter = tf.lite.TFLiteConverter.from_saved_model("saved")
+    # converter = tf.lite.TFLiteConverter.from_keras_model(model)
+
+    converter.target_spec.supported_ops = [
+        tf.lite.OpsSet.TFLITE_BUILTINS,
+        tf.lite.OpsSet.SELECT_TF_OPS
+    ]
+
+    # converter.experimental_new_converter = True
+
+    # # Convert the model
+    # # post_training_quantize = True
+    tflite_model = converter.convert()
+
+    open(model_name, "wb").write(tflite_model)
+    print("Tflite model saved")
+
+
+if __name__ == '__main__':
+    # class_names_file = os.path.abspath("yolov3_tf2\cfg\\taco-10.names")
+    # cfg_file = os.path.abspath("yolov3_tf2\cfg\yolov3-10b-angle.cfg")
+    # weights_file = os.path.abspath(
+    #     "yolov3_tf2\weights\yolov3-taco.tf")
+
+    class_names_file = os.path.abspath("yolov3_tf2\cfg\\taco-10.names")
+    cfg_file = os.path.abspath("yolov3_tf2\cfg\\yolov3-10b-angle.cfg")
+    # weights_file = os.path.abspath(
+    #     "yolov3_tf2\checkpoints\yolov3.tf")
+
+    # class_names_file = os.path.abspath("yolov3_tf2\cfg\coco.names")
+    # cfg_file = os.path.abspath("yolov3_tf2\cfg\yolov3-tiny-prn.cfg")
+    weights_file = os.path.abspath(
+        "yolov3_tf2\weights\\yolov3.tf")
+
+    model_size = (416, 416, 3)
+    num_classes = 10
+    max_output_size = 10
+    max_output_size_per_class = 5
+    iou_threshold = 0.5
+    confidence_threshold = 0.3
+
+    # Change parameters value for different run modes
+    detect = True
+    conversion = False
+
+    yolo = YoloV3(cfg_file, model_size, num_classes,
+                  iou_threshold, confidence_threshold)
+    model = yolo.create_network()
+    model.load_weights(weights_file)  # .expect_partial()
+
+    # model.build(model.input.shape)
+
+    detect_image(detect, class_names_file, model_size, model)
+
+    if conversion:
+        tflite_model_name = "tiny-prn.tflite"
+        # save the model as .pb
+        print("Start saving model as pb....")
+        tf.saved_model.save(model, 'saved')
+        model.save('model.h5')
+        # # Create a converter and tflite model
+        print("End model conversion as pb....")
+        tflite_model_name = "yolov3.tflite"
+        convert(model, tflite_model_name)
---
+++ ModelYoloImpl/yolov3_tf2/utils.py
@@ -0,0 +1,105 @@
+import sys
+import numpy as np
+import cv2
+import tensorflow as tf
+
+
+def non_max_suppresion(inputs, model_size, max_output_size,
+                       max_output_size_per_class, iou_threshold, confidence_threshold):
+    bbox, confs, class_probs = tf.split(inputs, [4, 1, -1], axis=-1)
+    bbox = bbox / model_size[0]
+
+    bbox = tf.reshape(bbox, (bbox.shape[1], 4))
+    scores = tf.multiply(confs, class_probs)
+
+    classes = tf.keras.backend.argmax(scores, axis=-1)[0, :]
+    classes = tf.cast(classes, tf.float32)
+    scores = tf.keras.backend.max(scores, axis=-1)[0, :]
+
+    selected_indeces = tf.image.non_max_suppression(
+        boxes=bbox,
+        scores=scores,
+        max_output_size=max_output_size,
+        iou_threshold=iou_threshold,
+        score_threshold=confidence_threshold)
+
+    boxes = tf.gather(bbox, selected_indeces)  # (<10, 4)
+    classes = tf.gather(classes, selected_indeces)  # (<10)
+    scores = tf.gather(scores, selected_indeces)  # (<10)
+
+    pad = [[0, max_output_size - tf.shape(boxes)[0]], [0, 0]]
+    pad2 = [pad[0]]
+
+    boxes = tf.pad(boxes, pad, "CONSTANT")
+    classes = tf.pad(classes, pad2, "CONSTANT")
+    scores = tf.pad(scores, pad2, "CONSTANT")
+
+    boxes = tf.reshape(boxes, (1, tf.shape(boxes)[0], 4))
+    classes = tf.reshape(classes, (1, -1))
+    scores = tf.reshape(scores, (1, -1))
+
+    valid_detections = tf.shape(selected_indeces)
+
+    return boxes, classes, scores, valid_detections
+
+
+def resize_image(inputs, model_size):
+    inputs = tf.image.resize(inputs, model_size)
+    return inputs
+
+
+def transform_images(val, size):
+    val = tf.image.resize(val, (size, size))
+    val = val / 255
+    return val
+
+
+def load_class_names(file_name):
+    '''
+    Read class names from file.
+
+    Inputs:
+        file_name: string -> file path to class names
+    Return:
+        class_names: list -> a list with all class names
+    '''
+    with open(file_name, 'r') as file_stream:
+        class_names = file_stream.read().splitlines()
+    return class_names
+
+
+def output_boxes(inputs, model_size, max_output_size, max_output_size_per_class,
+                 iou_threshold, confidence_threshold):
+    center_x, center_y, width, height, confidence, classes = \
+        tf.split(inputs, [1, 1, 1, 1, 1, -1], axis=-1)
+
+    top_left_x = center_x - width / 2.0
+    top_left_y = center_y - height / 2.0
+    bottom_right_x = center_x + width / 2.0
+    bottom_right_y = center_y + height / 2.0
+
+    inputs = tf.concat([top_left_x, top_left_y, bottom_right_x,
+                        bottom_right_y, confidence, classes], axis=-1)
+
+    boxes_dicts = non_max_suppresion(inputs, model_size, max_output_size,
+                                     max_output_size_per_class, iou_threshold, confidence_threshold)
+    return boxes_dicts
+
+
+def draw_output(img, boxes, objectness, classes, nums, class_names):
+    '''
+    Draw rectangle on image from bbox.
+    '''
+    boxes, objectness, classes, nums = boxes[0], objectness[0], classes[0], nums[0]
+    boxes = np.array(boxes)
+
+    for i in range(int(nums)):
+        x1y1 = tuple(
+            (boxes[i, 0:2] * [img.shape[1], img.shape[0]]).astype(np.int32))
+        x2y2 = tuple(
+            (boxes[i, 2:4] * [img.shape[1], img.shape[0]]).astype(np.int32))
+        img = cv2.rectangle(img, (x1y1), (x2y2), (255, 0, 0), 2)
+        img = cv2.putText(img, '{} {:.4f}'.format(
+            class_names[int(classes[i])], objectness[i]),
+            (x1y1), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
+    return img
